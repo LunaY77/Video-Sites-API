3be2a0adca3bc0384d8b8d469895f3009289ceea
IFLOVE <2307984361@qq.com>
2024-09-01 21:54:45 +0800

    完成登录、注册、重置密码、退出登录接口

---
+++ src/test/java/com/iflove/videositesapi/VideoSitesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iflove.videositesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSitesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
